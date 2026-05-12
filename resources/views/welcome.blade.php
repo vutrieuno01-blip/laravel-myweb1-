58b2774f126e94c0352f8818285014cccad639a1
VuHaiTrieu <youremail@gmail.com>
2026-05-12 15:58:17 +0700

    Init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thông tin cá nhân</title>
+</head>
+<body>
+    <h1>THÔNG TIN CÁ NHÂN</h1>
+
+    <p>Họ tên: Vũ Hải Triều</p>
+    <p>MSSV: 2124110315</p>
+    <p>Lớp: ............</p>
+    <p>Ngành: Công nghệ thông tin</p>
+
+</body>
+</html>
